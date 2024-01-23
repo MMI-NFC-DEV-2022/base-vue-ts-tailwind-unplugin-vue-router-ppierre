77a03057833a0b3e5037c029e137b4c28297521e
pierre pracht <pierre.pracht@gmail.com>
2024-01-23 11:39:11 +0100

    install unplugin-vue-router

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(
+  createRouter({
+    history: createWebHistory()
+  })
+)
 
 app.mount('#app')
